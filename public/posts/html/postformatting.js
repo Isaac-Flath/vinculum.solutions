b01cc4f332ee351c311b6d0f2369dae115b26a62
Isaac Flath <isaac.flath@isaacMBP.local>
2021-09-05 08:58:31 -0400

    updates

--- public/posts/html/postformatting.js
+++ public/posts/html/postformatting.js
@@ -1,60 +1,54 @@
-function hide_cells() {
-    var divs = document.getElementsByTagName('div');
+var divs = document.getElementsByTagName('div');
+
+function get_divs_containing(text,divs) {
+    const out = []
+
     for (var i = 0, len = divs.length; i < len; ++i) {
-        // console.log("Outer Loop");
-        // console.log(i);
 
-        var tmpbool = divs[i].parentElement.id == "notebook-container";
-        // console.log(tmpbool);
-        if (tmpbool) {
-
-            var tmpbool = divs[i].classList.value == "cell border-box-sizing code_cell rendered";
-            // console.log(tmpbool);
-            if (tmpbool) {
-
-
-                var tmpbool = divs[i].innerHTML.indexOf("#hide_input") !== -1
-                // console.log(tmpbool);
-                if (tmpbool) {
-
-
-                    for (var j = 0, innerlen = divs[i].children.length; j < innerlen; j++) {
-                        // console.log("InnerLoop : Input");
-                        // console.log(j);
-
-                        var tmpbool = divs[i].children[j].classList.value == "input"
-                        // console.log(tmpbool);
-                        if (tmpbool) {
-
-                            divs[i].children[j].style.display = "none";
-                            console.log("Hid Input");
-
-                        }
-                    }
-                }
-
-                var tmpbool = divs[i].innerHTML.indexOf("#hide_output") !== -1;
-                // console.log(tmpbool);
-                if (tmpbool) {
-
-                    for (var j = 0, innerlen = divs[i].children.length; j < innerlen; j++) {
-                        // console.log("InnerLoop : Output");
-                        // console.log(j);
-
-                        var tmpbool = divs[i].children[j].classList.value == "output_wrapper";
-                        // console.log(tmpbool);
-                        if (tmpbool) {
-
-                            divs[i].children[j].style.display = "none";
-                            console.log("Hid Output");
-                        }
-
-                    }
-                }
-            }
+        var tmpbool1 = divs[i].parentElement.id == "notebook-container";
+        var tmpbool2 = divs[i].classList.value == "cell border-box-sizing code_cell rendered";
+        var tmpbool3 = divs[i].innerHTML.indexOf(text) !== -1;
+        if (tmpbool1 && tmpbool2 && tmpbool3) {
+            var tmpdict = {};
+            for (var j = 0, innerlen = divs[i].children.length; j < innerlen; j++) {
+                if(divs[i].children[j].classList.value == "input"){tmpdict["input"] = divs[i].children[j]}
+                if(divs[i].children[j].classList.value == "output_wrapper"){tmpdict["output"] = divs[i].children[j]}}
+            out.push(tmpdict)
         }
     }
-};
+    return out
+}
 
-hide_cells();
+function hide_inputs(divs) {
+    // Takes #hide_input flags and hides input only
+    var hide_input_cells = get_divs_containing("#hide_input",divs)
+    
+    for (var i = 0, len = hide_input_cells.length; i < len; ++i) {
+        hide_input_cells[i]["input"].style.display = "none"}}
 
+function hide_outputs(divs) {
+    // Takes #hide_output flags and hides output only
+    var hide_input_cells = get_divs_containing("#hide_output",divs)
+    
+    for (var i = 0, len = hide_input_cells.length; i < len; ++i) {
+        hide_input_cells[i]["input"].style.display = "none"}}
+
+function hide_all(divs) {
+    // takes #hide flags and hides inputs and outputs if they exist
+    var hide_input_cells = get_divs_containing("#hide",divs)
+    
+    for (var i = 0, len = hide_input_cells.length; i < len; ++i) {
+        var tmpvar = hide_input_cells[i]["input"]
+
+        if(tmpvar.innerHTML.indexOf("#hide_input") == -1 && tmpvar.innerHTML.indexOf("#hide_output") == -1){ 
+            tmpvar.style.display = "none"
+            if("output" in hide_input_cells[i]){
+                hide_input_cells[i]["output"].style.display = "none";}
+        }
+    }
+    
+}
+
+hide_inputs(divs);
+hide_outputs(divs);
+hide_all(divs);
